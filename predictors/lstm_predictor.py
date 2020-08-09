bac391d484667523a19a07e1778fa098ef96f4ae
Suraj Shrestha <syuraj@gmail.com>
2020-08-08 23:59:41 -0400

    run new model

--- predictors/lstm_predictor.py
+++ predictors/lstm_predictor.py
@@ -1,150 +1,103 @@
-# %% Import libraries
-import os
-
-os.environ["PYTHONHASHSEED"] = "0"
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-os.environ["TF_CUDNN_USE_AUTOTUNE"] = "0"
-
-import sys
-
-sys.path.insert(0, "../")
-import pandas as pd
-import numpy as np
-import random as rn
-import tensorflow as tf
-
-rn.seed(0)
-np.random.seed(0)
-tf.random.set_seed(0)
-
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+#%%  Import the libraries
 import math
+import numpy as np
+import pandas as pd
+from environs import Env
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
-from utilities.get_abs_path import get_abs_path
 
+plt.style.use("fivethirtyeight")
 
-# %% Import data
+env = Env()
+env.read_env()
+DATABASE_URL = env("DATABASE_URL")
 
-cols_list = [
-    "startEpochTime",
-    "openPrice",
-    "highPrice",
-    "lowPrice",
-    "volume",
-    "closePrice",
-]
+engine = create_engine(DATABASE_URL)
+df = pd.read_sql(
+    "select close_price from stock_data order by datetime desc limit 100000", con=engine
+)
 
-stock_history = pd.read_csv(get_abs_path(__file__, "../data/TSLA.csv"))
-stock_history = stock_history[cols_list]
+df["Close"] = df["close_price"]
+data = df.filter(["Close"])
 
-scaler = MinMaxScaler()
-stock_history = scaler.fit_transform(stock_history)
-stock_history = pd.DataFrame(stock_history)
-# stock_history = stock_history.head(1000)
+dataset = data.values
 
-# %% Prepare training data
-TRAINING_LENGTH = math.ceil(0.90 * len(stock_history))
-training_data = stock_history.iloc[:TRAINING_LENGTH, :]
+#%% Get /Compute the number of rows to train the model on
+training_data_len = math.ceil(len(dataset) * 0.95)
 
-x_train, y_train = [], []
-BATCH_SIZE = 30
-NO_OF_INPUT_COLS = 5
+# Scale the all of the data to be values between 0 and 1
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
 
-for i in range(BATCH_SIZE, len(training_data) - 2):
-    x_train.append(
-        training_data.iloc[i - BATCH_SIZE : i, 0:NO_OF_INPUT_COLS].to_numpy().tolist()
-    )
-    y_train.append(
-        training_data.iloc[i : i + 1, NO_OF_INPUT_COLS : NO_OF_INPUT_COLS + 1]
-        .iloc[0, :]
-        .to_numpy()
-        .tolist()
-    )
+# Create the scaled training data set
+train_data = scaled_data[0:training_data_len, :]
 
+#%% Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+BATCH_SIZE = 60
+
+for i in range(BATCH_SIZE, len(train_data) - 6):
+    x_train.append(train_data[i - BATCH_SIZE : i, :])
+    y_train.append(train_data[i + 6, 0])
+
+#%% Convert x_train and y_train to numpy arrays
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-# %% Build Model
-regressor = Sequential()
-regressor.add(
-    LSTM(
-        units=4,
-        activation="relu",
-        return_sequences=True,
-        input_shape=(x_train.shape[1], x_train.shape[2]),
-    )
-)
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=100, activation="relu", return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=100, activation="relu", return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=100, activation="relu"))
-regressor.add(Dropout(0.2))
-regressor.add(Dense(units=1))
+# Reshape the data into the shape accepted by the LSTM
+# x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-regressor.compile(
-    optimizer="adam", loss="mean_squared_error", metrics=["mean_squared_error"]
-)
+#%% Build the LSTM network model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=False))
+model.add(Dense(units=25))
+model.add(Dense(units=1))
 
-# %% Train the model
-stoppingCallback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=5)
+# Compile the model
+model.compile(optimizer="adam", loss="mae")
 
-regressor.fit(
-    x_train,
-    y_train,
-    epochs=100,
-    batch_size=BATCH_SIZE,
-    callbacks=[stoppingCallback],
-    shuffle=False,
-)
+#%% Train the model
+history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=30)
 
-# %% Prepare Test Data & Run Prediction
-test_data = stock_history.iloc[TRAINING_LENGTH:, :]
+# Test data set
+test_data = scaled_data[training_data_len - BATCH_SIZE :, :]
 
-x_test, y_test = [], []
-
+#%% Create the x_test and y_test data sets
+x_test = []
+y_test = dataset[training_data_len:, :]
 for i in range(BATCH_SIZE, len(test_data)):
-    x_test.append(
-        test_data.iloc[i - BATCH_SIZE : i, 0:NO_OF_INPUT_COLS].to_numpy().tolist()
-    )
-    y_test.append(
-        test_data.iloc[i : i + 1, NO_OF_INPUT_COLS : NO_OF_INPUT_COLS + 1]
-        .iloc[0, :]
-        .to_numpy()
-        .tolist()
-    )
+    x_test.append(test_data[i - BATCH_SIZE : i, :])
 
+
+#%% Convert x_test to a numpy array
 x_test = np.array(x_test)
-y_test = np.array(y_test)
 
-#  %% Evaluate & Run Prediction
+#%% Getting the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)  # Undo scaling
 
-test_score, test_acc = regressor.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
-print(test_score, test_acc)
+#%% Calculate/Get the value of RMSE
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+print(rmse)
 
-y_predict = regressor.predict(x_test)
+# train = data[:training_data_len]
+valid = data[training_data_len:]
+valid["Predictions"] = predictions
 
-# x_test = x_test / scaler.scale_[0] + scaler.data_min_[0]
-y_predict = (
-    y_predict / scaler.scale_[NO_OF_INPUT_COLS] + scaler.data_min_[NO_OF_INPUT_COLS]
-)
-y_test = y_test / scaler.scale_[NO_OF_INPUT_COLS] + scaler.data_min_[NO_OF_INPUT_COLS]
-
-
-# %%
+#%% Visualize the data
 plt.close()
 fig = plt.gcf()
-plt.plot(y_test, color="red", label="Real Price")
-plt.plot(y_predict, color="blue", label="Predicted Price")
-title = "TSLA Price Prediction: test_score: {test_score:.3f}, test_acc: {test_acc:.3f}".format(
-    test_score=test_score, test_acc=test_acc
-)
 
+plt.plot(y_test[-100:], color="red", label="Real Price")
+plt.plot(predictions[-100:], color="blue", label="Predicted Price")
+title = "TSLA Price Prediction:"
 plt.title(title)
 plt.xlabel("Time")
 plt.ylabel("Price")
-plt.legend()
-fig.savefig(get_abs_path(__file__, "../charts/tsla-prediction.png"), dpi=fig.dpi)
+plt.legend(loc="upper right")
+fig.savefig("../charts/tsla-prediction.png", dpi=fig.dpi)
 plt.show()
