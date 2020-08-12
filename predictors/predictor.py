6c3cfa14759dbadb61d4c43e8f0fd46ba36602e5
Suraj Shrestha <syuraj@gmail.com>
2020-08-12 01:08:41 -0400

    run prediction from saved model

--- predictors/predictor.py
+++ predictors/predictor.py
@@ -25,7 +25,7 @@ class Predictor:
         symbol = "TSLA"
         print(f"Running prediction for {symbol}")
 
-        filename = get_abs_path(__file__, "./lstm_predictor.py")
+        filename = get_abs_path(__file__, "./predict_using_saved_model.py")
 
         with open(filename, "rb") as source_file:
             code = compile(source_file.read(), filename, "exec")
